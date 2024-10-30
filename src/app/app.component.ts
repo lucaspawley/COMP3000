fc3c981a643561a772986310ac4f6b9c42941b8d
Lucas <lucas.pawley@students.plymouth.ac.uk>
2024-10-30 14:19:38 +0000

    DEV-9 Added Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserLoginComponent } from '../user-login/user-login.component';
+import { UserLoginComponent } from '../components/account/user-login/user-login.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { UserLoginComponent } from '../components/account/user-login/user-login.component';
+import { MainPageComponent } from '../components/main-page/main-page.component';
+import { AccountPageComponent } from '../components/account/account-page/account-page.component';
+import { FavRecipePageComponent } from '../components/recipes/fav-recipe-page/fav-recipe-page.component';
+import { FindRecipePageComponent } from '../components/recipes/find-recipe-page/find-recipe-page.component';
+import { AssistantPageComponent } from '../components/assistant/assistant-page/assistant-page.component';
+import { ShoppingListsPageComponent } from '../components/shopping-lists-page/shopping-lists-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UserLoginComponent },
+  {
+    path: 'main-page',
+    component: MainPageComponent,
+    children: [
+      { path: 'account', component: AccountPageComponent },
+      { path: 'favourite-recipes', component: FavRecipePageComponent },
+      { path: 'find-recipes', component: FindRecipePageComponent },
+      { path: 'assistant', component: AssistantPageComponent },
+      { path: 'shopping-lists', component: ShoppingListsPageComponent },
+    ],
+  },
+];
