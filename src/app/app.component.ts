2216ca0c325dbe42238bc7f255e1e77e6cce7272
Lucas <lucas.pawley@students.plymouth.ac.uk>
2024-10-30 10:39:27 +0000

    DEV-9 Added user login component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserLoginComponent } from '../user-login/user-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserLoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
