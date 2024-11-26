2081acdeb5a65c2b217f7a0de8e12656b242b53e
Lucas <lucas.pawley@students.plymouth.ac.uk>
2024-11-26 13:10:25 +0000

    DEV-12 Added Account service to front end

--- angular-app/src/components/account/user-login/user-login.component.ts
+++ angular-app/src/components/account/user-login/user-login.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { AccountService } from '../../../services/account.service';
+import { Account } from '../types/types';
 
 @Component({
   selector: 'app-user-login',
@@ -13,14 +22,36 @@ import { InputTextModule } from 'primeng/inputtext';
     InputGroupModule,
     InputGroupAddonModule,
     InputTextModule,
+    PasswordModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.scss',
 })
-export class UserLoginComponent {
-  constructor(private router: Router) {}
+export class UserLoginComponent implements OnInit {
+  loginInForm!: FormGroup;
+
+  currentAccount: Account | undefined;
+
+  constructor(private router: Router, private fb: FormBuilder, private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.loginInForm = this.fb.group({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
 
   navigateLogin() {
-    this.router.navigate(['home']);
+    this.accountService
+      .login(
+        this.loginInForm.get('username')?.value,
+        this.loginInForm.get('password')?.value
+      )
+      .subscribe((response) => {
+        this.currentAccount = response;
+        console.log(response);
+        if (response !== undefined) this.router.navigate(['home']);
+      });
   }
 }
