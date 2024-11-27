7446ed772c7be0aaf7ff84a0232025fc8a3097e5
Lucas <lucas.pawley@students.plymouth.ac.uk>
2024-11-27 16:28:56 +0000

    DEV-13 Updated Types

--- angular-app/src/components/account/types/types.ts
+++ angular-app/src/components/account/types/types.ts
@@ -1,21 +1,64 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.24.612 on 2024-11-11 12:01:32.
+// Generated using typescript-generator version 2.24.612 on 2024-11-27 15:18:00.
 
-export class Account {
-    accountId?: number;
-    username?: string;
+export class Account implements UserDetails {
     password?: string;
+    enabled?: boolean;
+    username?: string;
+    accountNonLocked?: boolean;
+    authorities?: GrantedAuthority[];
+    credentialsNonExpired?: boolean;
+    accountNonExpired?: boolean;
+    accountId?: number;
     email?: string;
     tasteProfile?: TasteProfile;
 }
 
 export class Allergy {
     allergyId?: number;
-    allergy_name?: string;
+    allergyName?: string;
 }
 
 export class TasteProfile {
     tasteProfileId?: number;
     allergies?: Allergy[];
+}
+
+export class Method {
+    methodId?: number;
+    method_step?: number;
+    method_description?: string;
+}
+
+export class Recipe {
+    recipe_id?: number;
+    recipe_name?: string;
+    recipe_rating?: number;
+    recipe_serves?: number;
+    allergies?: Allergy[];
+    ingredients?: Ingredient[];
+    methods?: Method[];
+}
+
+export class Ingredient {
+    ingredientName?: string;
+    ingredient_id?: number;
+}
+
+export interface UserDetails extends Serializable {
+    password?: string;
+    enabled?: boolean;
+    username?: string;
+    accountNonLocked?: boolean;
+    authorities?: GrantedAuthority[];
+    credentialsNonExpired?: boolean;
+    accountNonExpired?: boolean;
+}
+
+export interface GrantedAuthority extends Serializable {
+    authority?: string;
+}
+
+export interface Serializable {
 }
