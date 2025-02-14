9a70a771cf3696ef2e8788dfb43e4e6328c11b6d
Lucas <lucas.pawley@students.plymouth.ac.uk>
2025-02-14 14:28:17 +0000

    DEV-20 Added Rating to front end

--- angular-app/src/components/recipes/recipe/recipe.component.ts
+++ angular-app/src/components/recipes/recipe/recipe.component.ts
@@ -6,40 +6,89 @@ import { RecipeService } from '../../../services/recipe.service';
 import { Recipe } from '../../types/types';
 import { ChipModule } from 'primeng/chip';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { RatingModule } from 'primeng/rating';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-recipe',
-  imports: [CardModule, ImageModule, ChipModule, ButtonModule],
+  imports: [
+    CardModule,
+    ImageModule,
+    ChipModule,
+    ButtonModule,
+    DialogModule,
+    RatingModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss',
 })
 export class RecipeComponent implements OnInit {
   recipe: Recipe | undefined;
+  dialogVisible: boolean = false;
+
+  ratingFormGroup!: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-    private sanitizer: DomSanitizer
+    private accountService: AccountService,
+    private sanitizer: DomSanitizer,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
+    this.getRecipe();
+  }
+
+  sanitizeImage(base64: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(
+      `data:image/jpeg;base64,${base64}`
+    );
+  }
+
+  getRecipe() {
     let recipeId = '';
     this.route.url.subscribe((url) => (recipeId = url[0].path));
+
     this.recipeService.getRecipe(recipeId).subscribe((result) => {
       this.recipe = {
         ...result,
         imageSafeUrl: this.sanitizeImage(result.imageBase64),
       };
-      console.log(this.recipe);
+    });
+
+    this.ratingFormGroup = this.fb.group({
+      recipeId: recipeId,
+      accountId: this.accountService.currentAccountId,
+      recipeRating: 0,
     });
   }
 
-  sanitizeImage(base64: string): SafeUrl {
-    return this.sanitizer.bypassSecurityTrustUrl(`data:image/jpeg;base64,${base64}`);
+  rateRecipe() {
+    this.recipeService
+      .rateRecipe(this.ratingFormGroup.value)
+      .subscribe((result) => {
+        this.recipe = {
+          ...result,
+          imageSafeUrl: this.sanitizeImage(result.imageBase64),
+        };
+      });
+    this.getRecipe();
+    this.showDialog();
   }
-  
-  rateRecipe() {}
+
+  showDialog() {
+    this.dialogVisible = !this.dialogVisible;
+  }
 
   addToList() {}
 
