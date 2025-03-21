b1d88249473dd16ea3155ee26b27170ae3389cea
Lucas <lucas.pawley@students.plymouth.ac.uk>
2025-03-21 09:46:09 +0000

    DEV-23 Created endpoints

--- sousChefAPI/src/main/java/com/springboot/sousChefAPI/repository/RecipeRepository.java
+++ sousChefAPI/src/main/java/com/springboot/sousChefAPI/repository/RecipeRepository.java
@@ -2,6 +2,7 @@ package com.springboot.sousChefAPI.repository;
 
 import com.springboot.sousChefAPI.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,10 @@ import java.util.List;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     List<Recipe> findByAccountID(int accountID);
+
+    @Query("SELECT r FROM Recipe r ORDER BY r.recipe_rating DESC LIMIT 10")
+    List<Recipe> findTop10ByRating();
+
+    @Query(value = "SELECT * FROM recipes ORDER BY RAND() LIMIT 12", nativeQuery = true)
+    List<Recipe> findRandomRecipes();
 }
